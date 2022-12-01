bf15c93402f2e17f3919db2fb9b60aed8b49f90d
setouchi-h <setouchi.kazuki@gmail.com>
2022-12-01 12:36:52 +0900

    initial commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    // calldata: temporary, cannot be modified
+    // memory: temporary, can be modified
+    // storage: permanent, can be modified
+    // string is secretly array, so memory is needed
+    // but solidity automatically knows where are you in uint256 going to be
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
